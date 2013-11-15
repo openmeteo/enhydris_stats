7b2e421d65fdc9e459632d73291107af3227c3ca
Antonis Christofides <antonis@wikical.com>
2013-11-15 16:33:48 +0200

    Improvements

--- enhydris_stats/views.py
+++ enhydris_stats/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils.translation import ugettext as _
 
 from enhydris.hcore import models
 
@@ -12,11 +13,14 @@ def home(request):
     data = []
     for year in years:
         dataitem = {'year': year}
-        dataitem['nstations'] = models.Gentity.objects.extra(
+        base_queryset = models.Gentity.objects.extra(
             where=["id in "
                    "(SELECT gentity_id FROM hcore_timeseries t "
                    "WHERE timeseries_end_date(t.id) >= '%s-01-01')"],
-            params=[year]).count()
+            params=[year])
+        dataitem['nstations'] = [
+            {'name': _(u'Total'), 'value': base_queryset.count()},
+        ]
         data.append(dataitem)
     return render_to_response('enhydris_stats/index.html',
                               {'data': data,
