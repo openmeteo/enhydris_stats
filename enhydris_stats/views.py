59ea8738516d9b4625ae50ffb2de1e6b32477af6
Antonis Christofides <antonis@wikical.com>
2013-11-15 17:09:19 +0200

    Columns by water division

--- enhydris_stats/views.py
+++ enhydris_stats/views.py
@@ -5,6 +5,14 @@ from django.utils.translation import ugettext as _
 from enhydris.hcore import models
 
 
+def get_columns(base_queryset):
+    result = [{'name': wd.id % 100,
+               'value': base_queryset.filter(water_division__id=wd.id).count()}
+              for wd in models.WaterDivision.objects.order_by('id')]
+    result.append({'name': _(u'Total'), 'value': base_queryset.count()})
+    return result
+
+
 def home(request):
     try:
         years = [int(x) for x in request.GET.get('years', '').split(',')]
@@ -12,18 +20,20 @@ def home(request):
         years = []
     data = []
     for year in years:
-        dataitem = {'year': year}
         base_queryset = models.Gentity.objects.extra(
             where=["id in "
                    "(SELECT gentity_id FROM hcore_timeseries t "
                    "WHERE timeseries_end_date(t.id) >= '%s-01-01')"],
             params=[year])
-        dataitem['nstations'] = [
-            {'name': _(u'Total'), 'value': base_queryset.count()},
-        ]
-        data.append(dataitem)
+        data.append({
+            'heading': _('Number of stations with data for {0} or later'
+                         ).format(year),
+            'columns': get_columns(base_queryset),
+        })
+    data.append({
+        'heading': _('Number of stations in database'),
+        'columns': get_columns(models.Station.objects),
+    })
     return render_to_response('enhydris_stats/index.html',
-                              {'data': data,
-                               'nstations': models.Station.objects.count(),
-                               },
+                              {'data': data},
                               context_instance=RequestContext(request))
