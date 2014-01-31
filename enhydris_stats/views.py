3b00532f9ea455a88eca7eb3c1f0caaab0ec2213
Antonis Christofides <antonis@wikical.com>
2014-01-31 18:58:04 +0200

    Filter stations list by minimum end year

--- enhydris_stats/views.py
+++ enhydris_stats/views.py
@@ -52,6 +52,13 @@ class StationsListView(TemplateView):
         context['stations'] = self._get_stations()
         return context
 
+    def get(self, request, *args, **kwargs):
+        try:
+            self.min_end_year = int(request.GET.get('year', '0'))
+        except ValueError:
+            self.min_end_year = 0
+        return super(StationsListView, self).get(request, *args, **kwargs)
+
     def _get_stations(self):
         all_stations = models.Gentity.objects.all()
         stations = []
@@ -59,7 +66,8 @@ class StationsListView(TemplateView):
             variables = {}  # Variable name to end year mapping
             ts = models.Timeseries.objects.filter(gentity=station).extra(
                 select={'t_end_date': 'timeseries_end_date(id)'},
-                where=['timeseries_end_date(id) IS NOT NULL'])
+                where=['EXTRACT(YEAR FROM timeseries_end_date(id)) >= {}'
+                       .format(self.min_end_year)])
             for t in ts:
                 variables[t.variable.descr] = max(
                     t.t_end_date.year, variables.get(t.variable.descr, 0))
